01247753db9ccdf7bdfe368b88071f8ed73cd872
Sarthak Dalal <137261279+sarthakdalal@users.noreply.github.com>
2024-09-04 18:28:19 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailFormComponent } from './payment-detais/payment-detail-form/payment-detail-form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PaymentDetailsComponent, PaymentDetailFormComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
---
+++ src/app/payment-details/payment-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PaymentDetailFormComponent } from '../payment-detais/payment-detail-form/payment-detail-form.component';
+@Component({
+  selector: 'app-payment-details',
+  standalone: true,
+  imports: [PaymentDetailFormComponent],
+  templateUrl: './payment-details.component.html',
+  styles: ``
+})
+export class PaymentDetailsComponent {
+
+}
